d40fd8adfd2c6c3a61dcf2c5d93a48ed082a2678
Brooks <brooks@karlik.org>
2022-04-19 09:15:23 -0700

    comments on exporting values

--- src/io_utils.jl
+++ src/io_utils.jl
@@ -62,6 +62,13 @@ module IoUtils
 		ny = writer.dims.ny
 
 
+		#
+		# First, use the indexers iu and iv to copy the velocity elements from q to 
+		# their respective matricies. 
+		#
+		# Here, u_tmp is (nx-1, ny) and v_tmp is (nx, ny-1)
+		# these are the same shapes that the iu, iv matricies are 
+		#
 		for i = 1:nx-1
 			for j = 1:ny
 				writer.u_tmp[i,j] = q[writer.iu[i,j]]
@@ -74,21 +81,48 @@ module IoUtils
 			end
 		end
 
+		# we can copy the pressure values _directly_ into the p matrix because 
+		# p is size (nx, ny) and the values are already cell centered
 		for i = 1:nx
 			for j = 1:ny
 				writer.p[i,j] = p[writer.ip[i,j]]
 			end
 		end
 
+		#
+		# In the u direction, to get the cell centered values we have to average nodes on the left
+		# and right of a cell.
+		#
+	
+		# in the domain (not on the boundaries), just average left and right values to get
+		# cell centered values
 		writer.u[2:nx-1, 1:ny] = (writer.u_tmp[2:nx-1, 1:ny] .+ writer.u_tmp[1:nx-2, 1:ny]) ./ 2
+		# average the nodes on the far left with the left boundary condition 
+		# for cell centered values of i = 1, j = 1:ny
 		writer.u[1, 1:ny] = (writer.u_tmp[1, 1:ny] + writer.bcs.u_l[1:ny]) ./ 2
+		# average the nodes on the far right with the right boundary condition 
+		# to get cell centered values of i = nx, j = 1:ny
 		writer.u[nx, 1:ny] = (writer.u_tmp[nx-1, 1:ny] + writer.bcs.u_r[1:ny]) ./ 2
 
+		#
+		# In the v direction, cell centered values are the average of nodes above and below a cell
+		#
+
+		# in the domain (not on the boundaries), just average top and bottom values to get
+		# cell centered values
 		writer.v[1:nx, 2:ny-1] = (writer.v_tmp[1:nx, 2:ny-1] .+ writer.v_tmp[1:nx, 1:ny-2]) ./ 2
+		# average the nodes on the bottom with the bottom boundary condition 
+		# for cell centered values of i = 1:nx, j =1
 		writer.v[1:nx, 1] = (writer.v_tmp[1:nx, 1] + writer.bcs.v_b[1:nx]) ./ 2
+		# average the nodes on the top with the top boundary condition
+		# for cell centered values of i = 1:nx, j =ny
 		writer.v[1:nx, ny] = (writer.v_tmp[1:nx, ny-1] + writer.bcs.v_t[1:nx]) ./ 2
 
-		# write the results to the output arrays for post processessing
+		#
+		# write the results to the output arrays for post processessing.
+		# these arrays are HDF5 and will simply be written to disk for plotting
+		# in a notebook
+		# 
 		writer.velocity_dset[step, 1, :, :] = writer.u
 		writer.velocity_dset[step, 2, :, :] = writer.v
 		writer.pressure_dset[step, :, :] = writer.p
